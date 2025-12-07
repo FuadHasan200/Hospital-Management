ddb7c4429ac26389e0f5abcee51b19f48b8e6fca
MD Fuad <fuadhasa199@gmail.com>
2025-12-07 11:23:51 +0600

    requirement added

--- hospital/settings.py
+++ hospital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gssrw8mom&_nrjgtuciq7fn-!hg$n!*^83bd&+8mq*(3s)@(in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
